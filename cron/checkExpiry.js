a47b631aade1c52af322218bcc0c1187b6511038
Whitecircle-grp <whitecircle768@gmail.com>
2025-11-12 00:53:56 +0530

    Update checkExpiry.js

--- cron/checkExpiry.js
+++ cron/checkExpiry.js
@@ -1,7 +1,7 @@
 const cron = require("node-cron");
 const sendExpiryReminders = require("../utils/sendExpiryReminders");
 
-cron.schedule("50 0 * * *", async () => {
+cron.schedule("55 0 * * *", async () => {
   await sendExpiryReminders();
 });
 
