ab7c6dee4d27b5484af6dc1e7b879ba144a0e1f8
Whitecircle-grp <whitecircle768@gmail.com>
2025-12-15 22:42:38 +0530

    fix: hindi alert change

--- utils/sendWhatsapp.js
+++ utils/sendWhatsapp.js
@@ -8,7 +8,7 @@ async function sendWhatsapp(to, vehicleNumber, upcomingExpiries) {
     const expiryList = upcomingExpiries.join(' || ');
 
   //  const expiryList = upcomingExpiries.join('%0A');
-    const url = `https://bhashsms.com/api/sendmsgutil.php?user=white_circle&pass=123456&sender=BUZWAP&phone=${ph}&text=vehicle_expiry_alert&priority=wa&stype=normal&params=${vehicleNumber},${expiryList}`;
+    const url = `https://bhashsms.com/api/sendmsgutil.php?user=white_circle&pass=123456&sender=BUZWAP&phone=${ph}&text=vehicle_expiry_alert_hindi&priority=wa&stype=normal&params=${vehicleNumber},${expiryList}`;
 
     console.log("Final URL:", url);
 
